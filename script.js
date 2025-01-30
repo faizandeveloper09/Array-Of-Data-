188c047f7eaebebc820fc9dec6791eca89d0d08c
SYED FAIZAN ALI <faizanbhai0409@gmail.com>
2025-01-30 09:22:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+let allQuestion = [
+
+   {
+     question : "HTML Stand for?",
+     option : [
+        
+        {answer : "Hypertext Makeup Language", iscorrect : false},
+        { answer : "Hypertext Make Language", iscorrect : false},
+        {answer : "Hypertext Markp Language", iscorrect : true}
+        
+     ]
+   },
+
+   {
+    question : "UI Stand for?",
+    option : [
+       
+       {answer : "User Interface", iscorrect : true},
+       { answer : "User Enetrface", iscorrect : false},
+       {answer : "User Interfacial", iscorrect : false}
+       
+    ]
+  },
+
+
+  {
+    question : "UX Stand for?",
+    option : [
+       
+       {answer : "User Experience", iscorrect : true},
+       { answer : "User Xperience", iscorrect : false},
+       {answer : "User Experian", iscorrect : false}
+       
+    ]
+  },
+
+
+]
