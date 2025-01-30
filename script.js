e2b642a6e7cac5b90e34582acbac248dfa0f0907
SYED FAIZAN ALI <faizanbhai0409@gmail.com>
2025-01-30 11:08:41 +0500

    second update

--- script.js
+++ script.js
@@ -3,38 +3,64 @@
 let allQuestion = [
 
    {
-     question : "HTML Stand for?",
-     option : [
-        
-        {answer : "Hypertext Makeup Language", iscorrect : false},
-        { answer : "Hypertext Make Language", iscorrect : false},
-        {answer : "Hypertext Markp Language", iscorrect : true}
-        
-     ]
+      question: "HTML Stand for?",
+      option: [
+
+         { answer: "Hypertext Makeup Language", iscorrect: false },
+         { answer: "Hypertext Make Language", iscorrect: false },
+         { answer: "Hypertext Markp Language", iscorrect: true }
+
+      ]
    },
 
    {
-    question : "UI Stand for?",
-    option : [
-       
-       {answer : "User Interface", iscorrect : true},
-       { answer : "User Enetrface", iscorrect : false},
-       {answer : "User Interfacial", iscorrect : false}
-       
-    ]
-  },
+      question: "UI Stand for?",
+      option: [
+
+         { answer: "User Interface", iscorrect: true },
+         { answer: "User Enetrface", iscorrect: false },
+         { answer: "User Interfacial", iscorrect: false }
+
+      ]
+   },
 
 
-  {
-    question : "UX Stand for?",
-    option : [
-       
-       {answer : "User Experience", iscorrect : true},
-       { answer : "User Xperience", iscorrect : false},
-       {answer : "User Experian", iscorrect : false}
-       
-    ]
-  },
+   {
+      question: "UX Stand for?",
+      option: [
 
+         { answer: "User Experience", iscorrect: true },
+         { answer: "User Xperience", iscorrect: false },
+         { answer: "User Experian", iscorrect: false }
 
+      ]
+   },
 ]
+
+
+let index = 0
+function displayQuestion() {
+   document.getElementById("displayques").innerHTML = allQuestion[index].question
+
+   let btn = ""
+   allQuestion[index].option.forEach((opt) => {
+      btn += `<button class="btn btn-outline-warning text-dark">${opt.answer}</button>`
+   })
+   document.getElementById("btn").innerHTML = btn
+}
+
+
+function next() {
+   index++
+   document.getElementById("displayques").innerHTML = allQuestion[index].question
+   displayQuestion()
+}
+
+
+function back() {
+   index--
+   document.getElementById("displayques").innerHTML = allQuestion[index].question
+   displayQuestion()
+}
+
+displayQuestion()
